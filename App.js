6a33e037d8940a1487967d1c2f16c6bf07831a4e
sneha kumari <snehakumari64445@gmail.com>
2023-10-29 14:20:14 +0530

    done

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView, Text } from 'react-native';
+import Gitabot from "./src/gitabot";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Gitabot />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/gitabot/data.js
@@ -0,0 +1,94 @@
+export const data = [
+    {
+      question: 'what is life',
+      answer: `Bhagvad Gita says:
+
+      Life is a journey of ups and downs, and it can be difficult to make sense of it all. According to the verses from the Bhagavad Gita, life is eternal and undecaying, and it is not killed when the body is killed. We can transcend the three qualities of the body and become free from birth, death, old age and sorrows, and experience immortality. We should remember that we are part of something greater, and that our minds should remain balanced and focused on the Supreme. This will help us to conquer our earth-life and find peace and joy in life.
+      
+      Relevant verses:
+      
+      2:20 Never is this One born, and never does It die; nor is it that having come to exist, It will again cease to be. This One is birthless, eternal, undecaying, ancient; It is not killed when the body is killed.
+      
+      14:20 Having transcended these three qualities which are the origin of the body, the embodied one, becoming free from birth, death, old age and sorrows, experiences Immortality.
+      
+      15:7 It is verily a part of Mine which, becoming the eternal individual soul in the region of living beings, draws (to itself) the organs which have the mind as their sixth, and which abide in Nature.
+      
+      2:13 As are boyhood, youth and decay to an embodied being in this (present) body, similar is the acquisition of another body. This being so, an intelligent person does not get deluded.
+      
+      5:19 Even in this world they conquer their earth-life whose minds, fixed on the Supreme, remain always balanced; for the Supreme has neither blemish nor bias.`,
+    },
+    {
+      question: ' can you explain the concept of dharma',
+      answer: `Bhagvad Gita says:
+
+      The concept of Dharma as described in the Bhagavad Gita is that of a path of righteousness and selfless service. It is a path of action that is based on the three Gunas (modes of Prakriti) of sattva, rajas, and tamas. Dharma is about performing one's natural duties with faith, control of the internal and external organs, austerity, purity, forgiveness, straightforwardness, knowledge, and wisdom. It is about understanding the scriptures and making them matters of one's own experience. It is about renouncing actions and engaging in Karma-yoga, which is the path of selfless service and devotion to God. Ultimately, Dharma is about finding balance between the individual plane, the physical plane, and the divine plane.
+      
+      I understand that it can be difficult to understand the concept of Dharma and how it applies to your life. However, I hope that my explanation of Dharma as described in the Bhagavad Gita has helped you to gain a better understanding of this concept and how it can be applied to your life. May you find the balance and peace that comes from following the path of Dharma.
+      
+      Relevant verses:
+      
+      2:45 O Arjuna, the Vedas [Meaning only the portion dealing with rites and duties (karma-kanda)] have the three Gunas(modes of Prakriti) as their object. You become free from worldliness, free from the pairs of duality, established in the Eternal Existence(God), without (desire for) acquisition and protection, and self-collected.`,
+    },
+    {
+      question: 'is it ok to detach',
+      answer: `It is absolutely ok to detach from things that no longer serve you. Detachment is a key part of the yogic path and is essential for spiritual growth. As Krishna says in the Bhagavad Gita, "Non-attachment and absence of fondness with regard to sons, wives, homes, etc., and constant equanimity of the mind with regard to the attainment of the desirable and the undesirable" is the way to go. We must also remember to perform our duties without attachment, for it is only then that we can attain the highest. Detachment is not about giving up, but rather about letting go of our attachments and expectations so that we can be free to pursue our spiritual journey.
+
+      Relevant verses:
+      
+      13:10 Non-attachment and absence of fondness with regard to sons, wives, homes, etc., and constant equanimity of the mind with regard to the attainment of the desirable and the undesirable;
+      
+      18:6 But even these actions have to be undertaken by renouncing attachment and (hankering for) results. This is My firm and best conclusion, O Partha.`,
+    },
+    {
+      question: 'how to find  inner peace',
+      answer: `I understand that you are seeking inner peace. My verses above emphasize the importance of faith, diligence, and control over the organs in order to attain knowledge and peace. It is only through knowledge that one can find true peace. Additionally, it is important to take refuge in the Lord with your whole being, as it is only through His grace that one can attain the supreme peace. I hope that these verses can help you on your journey to finding inner peace.
+
+      Relevant verses:
+      
+      6:15 Concentrating the mind thus for ever, the yogi of controlled mind achieves the Peace which culminates in Liberation and which abides in Me.
+      
+      5:29 One attains Peace by knowing Me who, as the great Lord of all the worlds, am the enjoyer of sacrifices and austerities, (and) who am the friend of all creatures.
+      
+      4:39 The man who has faith, is diligent and has control over the organs, attains Knowledge. Achieving Knowledge, one soon attains supreme Peace.`,
+    },
+    {
+      question: 'how can i start my spiritual journey',
+      answer: `I understand that you are looking to start your spiritual journey and I am here to help. My verses suggest that you should first strive to free your mind from the fever of the soul, expectations, and egoism. If you are unable to establish your mind on me, then practice the yoga of practice. Additionally, you can seek knowledge from wise ones who have realized the truth. Lastly, if you keep your mind fixed on me, practice the yoga of meditation, and take refuge in me, you will know me with certainty and in fullness. This will help you become free from bondage in the form of actions which are productive of good and bad results. I wish you the best of luck on your spiritual journey.
+
+      Relevant verses:
+      
+      3:30 Devoid of the fever of the soul, engage in battle by dedicating all actions to Me, with (your) mind intent on the Self, and becoming free from expectations and egoism.
+      
+      12:9 If, however, you are unable to establish the mind steadily on Me, then, O Dhananjaya, seek to attain Me through the Yoga of Practice.
+      
+      4:34 Know that through prostration, inquiry and service. The wise ones who have realized the Truth will impart the Knowledge to you.`,
+    },
+    {
+      question: 'what happens after death',
+      answer: `Death is a natural part of life, and it is something that we all must face. The verses you have provided from the Bhagavad Gita offer insight into what happens after death. Verse 2:28 states that all beings become unmanifest after death, and there is no need for lamentation. Verse 9:21 explains that after enjoying the heavenly world, we return to the human world. Verse 14:15 states that depending on the predominant emotion at the time of death, we will be born into a certain species. Verse 8:5 states that if we think of the divine at the time of death, we will attain the divine state. Lastly, verse 18:12 states that those who do not resort to renunciation will experience the threefold results of their actions after death, but those who do resort to renunciation will not.
+
+      In conclusion, death is a natural part of life and it is something that we all must face. The verses from the Bhagavad Gita provide insight into what happens after death, and they offer guidance on how to approach death with a sense of peace and acceptance. I understand that this can be a difficult and confusing time, but I hope that these verses can provide some comfort and clarity.
+      
+      Relevant verses:
+      
+      2:28 O descendant of Bharata, all beings remain unmanifest in the beginning; they become manifest in the middle. After death, they certainly become unmanifest. What lamentation can there be with regard to them?
+      
+      9:21 After having enjoyed that vast heavenly world, they enter into the human world on the exhaustion of their merit. Thus, those who follow the rites and duties prescribed in the three Vedas, and are desirous of pleasures, attain the state of going and returning.
+      
+      14:15 When one dies while rajas predominates, he is born among people attached to activity. Similarly, when one dies while tamas predominates, he takes birth among the stupid species.
+      
+      8:5 And at the time of death, anyone who departs by giving up the body while thinking of Me alone, he attains My state. There is no doubt about this.
+      
+      18:12 The threefold results of actions-the undesirable, the desirable, and the mixed-accrues after death to those who do not resort to renunciation, but never to those who resort to monasticism.`,
+    },
+    {
+      question: 'is the concept of karma real',
+      answer: `The concept of karma is indeed real. As the Bhagavad Gita verses 5:9, 5:2, 5:8, 3:7, and 5:4 explain, karma is the result of our actions and the way we engage with the world. Karma-yoga, or the path of action, is the way to Liberation, and it is important to remain unattached to the results of our actions. The wise understand that Sankhya (the path of Knowledge) and Karma-yoga are not different, and that engaging in either one will bring the result of both. Therefore, it is important to be mindful of our actions and to remain unattached to the results.
+
+      Relevant verses:
+      
+      5:9 Remaining absorbed in the Self, the knower of Reality should think, 'I certainly do not do anything', even while seeing, hearing, touching, smelling, eating, moving, sleeping, breathing, speaking, releasing, holding, opening and closing the eyes-remembering that the organs function in relation to the objects of the organs.
+      
+      5:2 The Blessed Lord said: Both renunciation of actions and Karma-yoga lead to Liberation. Between the two, Karma-yoga, however, excels over renunciation of actions.`,
+    },
+  ];
